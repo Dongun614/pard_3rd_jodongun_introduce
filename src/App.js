115b732a2217a0019076a945db67d65c93b92a0d
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2024-03-17 16:32:37 +0900

    start

--- src/App.js
+++ src/App.js
@@ -3,21 +3,52 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="whole-page">
+        <div className="side-side">
+          <div className="side-header">
+            <button className="side-header-button">
+              - 
+            </button>
+            Clock
+            <div>
+              <button type="button" className="side-header-button">
+                위
+              </button>
+              <button className="side-header-button">
+                아래
+              </button>
+            </div>
+          </div>
+          <div className="side-name">
+            Settings
+          </div>
+          <div className="side-img">
+          </div>
+        </div>
+
+
+        <div className="main-side">
+          <div className="main-header">
+            Program Manager
+            <div>
+                <button type="button" className="side-header-button">
+                  위
+                </button>
+                <button className="side-header-button">
+                  아래
+                </button>
+              </div>
+          </div>
+          <div className="main-name">
+            Pard_3rd_Web
+          </div>
+          <div className="content"></div>
+        </div>
+      </div>
+
+      <div className="footer">
+      </div>
     </div>
   );
 }
