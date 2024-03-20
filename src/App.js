c6e8e60cea27d9c34a4f3057290ca8228211753a
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2024-03-20 13:06:22 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -3,51 +3,72 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <div className="whole-page">
-        <div className="side-side">
-          <div className="side-header">
-            <button className="side-header-button">
-              - 
-            </button>
-            Clock
-            <div>
-              <button type="button" className="side-header-button">
-                위
-              </button>
-              <button className="side-header-button">
-                아래
-              </button>
+        <div className='side'>
+          <div className="side-side">
+            <div className='mycom_icon'>
+              <img src="/mycom.png" className="img_size" alt="mycom" />
+              내 컴퓨터
+            </div>
+            <div className='mycom_icon'>
+              <img src="/myfol.png" className="img_size" alt="myfolder" />
+              내 문서
+            </div>
+            <div className='mycom_icon'>
+              <img src="/inter.png" className="img_size" alt="internet" />
+              Internet<br />Explorer
+            </div>
+            <div className='mycom_icon'>
+              <img src="/network.png" className="img_size" alt="internet" />
+              네트워크<br />환경
+            </div>
+            <div className='mycom_icon'>
+              <img src="/trash.png" className="img_size" alt="internet" />
+              휴지통
             </div>
           </div>
-          <div className="side-name">
-            Settings
-          </div>
-          <div className="side-img">
+
+          <div className='side-side'>
+            <div className='mycom_icon'>
+              <img src="/myphoto.jpeg" className="img_size" alt="internet" />
+              Pard_3rd<br />Web
+            </div>
           </div>
         </div>
 
 
         <div className="main-side">
           <div className="main-header">
-            Program Manager
+            Pard_3rd_Web
             <div>
-                <button type="button" className="side-header-button">
-                  -
-                </button>
-                <button className="side-header-button">
-                  x
-                </button>
-              </div>
+              <button type="button" className="side-header-button">
+                -
+              </button>
+              <button className="side-header-button">
+                x
+              </button>
+            </div>
           </div>
           <div className="main-name">
-            Pard_3rd_Web
+            <h3>OOO 개발자</h3>
           </div>
-          <div className="content"></div>
+          <div className="content">
+            <img src="/myphoto.jpeg" className='myphoto' alt="myphoto" />
+            <div className='introduce'>
+              Jo Dong-un⭐<br/><br/>
+              INFJ 🥟<br /><br />
+              🏀, 🎵, 🎬
+
+            </div>
+          </div>
         </div>
       </div>
 
       <div className="footer">
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
